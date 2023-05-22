50639cd4712886e404819539cfabeba3b0f62e21
noornabi07 <noornabiprogram07@gmail.com>
2023-05-22 23:45:27 +0600

    ascending and descending and more search impelement

--- index.js
+++ index.js
@@ -23,18 +23,18 @@ const client = new MongoClient(uri, {
 });
 
 // verify token function here
-const verifyJWT = (req, res, next) =>{
+const verifyJWT = (req, res, next) => {
   console.log('verify hitting in jwt')
   console.log(req.headers.authorization);
   const authorization = req.headers.authorization;
-  if(!authorization){
-   return res.status(402).send({error: true, message: 'unauthorized your token'})
+  if (!authorization) {
+    return res.status(402).send({ error: true, message: 'unauthorized your token' })
   }
   const token = authorization.split(' ')[1];
   console.log('token inside verify:', token)
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) =>{
-    if(error){
-      return res.status(403).send({error: true, message: 'unAthorized access'})
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ error: true, message: 'unAthorized access' })
     }
     req.decoded = decoded;
     next()
@@ -50,64 +50,92 @@ async function run() {
     const servicesCollection = client.db('carDoctor').collection('services');
     const bookingCollection = client.db('carDoctor').collection('booking');
 
+
+    // search text code
+    const indexKeys = { services: 1, price: 1}
+    const indexOptions = { name: "servicesPrice" }
+    const result = await bookingCollection.createIndex(indexKeys, indexOptions)
+    console.log(result)
+
+    app.get('/bookingSearchServices/:text', async (req, res) => {
+      const searchText = req.params.text;
+      const result = await bookingCollection.find({
+        $or: [
+          { services: { $regex: searchText, $options: "i"} },
+          { price: { $regex: searchText, $options: "i"} }
+        ],
+      }).toArray();
+      res.send(result) 
+    })
+
     // jwt 
-    app.post('/jwt', (req, res) =>{
+    app.post('/jwt', (req, res) => {
       const user = req.body;
       console.log(user)
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
       console.log(token)
-      res.send({token})
+      res.send({ token })
     })
 
     // services route
-    app.get('/services', async(req, res) =>{
-        const cursor = servicesCollection.find();
-        const result = await cursor.toArray();
-        res.send(result)
+    app.get('/services', async (req, res) => {
+      const sort = req.query.sort;
+      const search = req.query.search;
+      // const query = {};
+      // const query = {price: {$lt: 100}}
+      const query = {title: {$regex: search, $options: 'i'}}
+      const options = {
+        sort:  {
+          "price" : sort === 'asc' ? 1 : -1
+        }
+      };
+      const cursor = servicesCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result)
     })
 
-    app.get('/services/:id', async(req, res) =>{
-        const id = req.params.id;
-        const query = {_id: new ObjectId(id)};
+    app.get('/services/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-        const options = {
-  
-            // Include only the `title` and `imdb` fields in the returned document
-            projection: { title: 1, price : 1, service_id: 1, img: 1 },
-          };
+      const options = {
 
-        const result = await servicesCollection.findOne(query, options);
-        res.send(result)
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id: 1, img: 1 },
+      };
+
+      const result = await servicesCollection.findOne(query, options);
+      res.send(result)
     })
 
     // bookings routes
 
-    app.get('/bookings', verifyJWT, async(req, res) =>{
+    app.get('/bookings', verifyJWT, async (req, res) => {
       const decoded = req.decoded;
       console.log('come back after verify', decoded);
 
-      if(decoded.email !== req.query.email){
-        return res.status(403).send({error: 1, message: 'forbidden verify'})
+      if (decoded.email !== req.query.email) {
+        return res.status(403).send({ error: 1, message: 'forbidden verify' })
       }
 
       let query = {}
-      if(req.query?.email){
-        query = {email: req.query.email}
+      if (req.query?.email) {
+        query = { email: req.query.email }
       }
       const result = await bookingCollection.find(query).toArray();
       res.send(result)
     })
 
-    app.post('/bookings', async(req, res) =>{
-      const booking  = req.body;
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
       console.log(booking)
       const result = await bookingCollection.insertOne(booking)
       res.send(result)
     })
 
-    app.patch('/bookings/:id', async(req, res) =>{
+    app.patch('/bookings/:id', async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) }
       const updatedBookings = req.body;
       console.log(updatedBookings)
 
@@ -121,9 +149,9 @@ async function run() {
       res.send(result)
     })
 
-    app.delete('/bookings/:id', async(req, res) =>{
+    app.delete('/bookings/:id', async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await bookingCollection.deleteOne(query)
       res.send(result)
     })
@@ -141,10 +169,10 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req, res) =>{
-    res.send('Car doctors is coming...')
+app.get('/', (req, res) => {
+  res.send('Car doctors is coming...')
 })
 
-app.listen(port, (req, res) =>{
-   console.log(`Car doctor server port is: ${port}`)
+app.listen(port, (req, res) => {
+  console.log(`Car doctor server port is: ${port}`)
 })
